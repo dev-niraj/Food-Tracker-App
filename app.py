5fb219293f5eb248f5cf47ef925430a3794d2f55
admin <admin-at-803553529221>
2019-12-24 16:32:05 +0530

    code refactored

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+
 from flask import Flask, render_template, g, request
+
 from database import get_db
 
 app = Flask(__name__)
@@ -24,7 +26,12 @@ def index():
         db.execute('insert into log_date (entry_date) values (?)', [database_date])
         db.commit()
 
-    cur = db.execute('select entry_date from log_date order by entry_date desc')
+    cur = db.execute('''select log_date.entry_date, sum(food.protein) as protein, sum(food.carbohydrates) as carbohydrates, sum(food.fat) as fat, sum(food.calories) as calories 
+                        from log_date 
+                        left join food_date on food_date.log_date_id = log_date.id 
+                        left join food on food.id = food_date.food_id 
+                        group by log_date.id order by log_date.entry_date desc''')
+
     results = cur.fetchall()
 
     date_results = []
@@ -33,6 +40,10 @@ def index():
         single_date = {}
 
         single_date['entry_date'] = i['entry_date']
+        single_date['protein'] = i['protein']
+        single_date['carbohydrates'] = i['carbohydrates']
+        single_date['fat'] = i['fat']
+        single_date['calories'] = i['calories']
 
         d = datetime.strptime(str(i['entry_date']), '%Y%m%d')
         single_date['pretty_date'] = datetime.strftime(d, '%B %d, %Y')
@@ -60,12 +71,12 @@ def view(date):
     food_cur = db.execute('select id, name from food')
     food_results = food_cur.fetchall()
 
-    log_cur = db.execute(
-        'select food.name, food.protein, food.carbohydrates, \
-        food.fat, food.calories from log_date join food_date on \
-        food_date.log_date_id = log_date.id join food on \
-        food.id = food_date.food_id where log_date.entry_date = ?',
-        [date])
+    log_cur = db.execute('''select food.name, food.protein, food.carbohydrates, food.fat, food.calories 
+                            from log_date 
+                            join food_date on food_date.log_date_id = log_date.id 
+                            join food on food.id = food_date.food_id 
+                            where log_date.entry_date = ?''', [date])
+
     log_results = log_cur.fetchall()
 
     totals = {}
@@ -80,7 +91,7 @@ def view(date):
         totals['fat'] += food['fat']
         totals['calories'] += food['calories']
 
-    return render_template('day.html', entry_date=date_result['entry_date'], pretty_date=pretty_date,
+    return render_template('day.html', entry_date=date_result['entry_date'], pretty_date=pretty_date, \
                            food_results=food_results, log_results=log_results, totals=totals)
 
 
